de1bc12961603ca5a4b08c3dd236e4d68daa9bc6
LoganCabanaw <lcabanaw@uwyo.edu>
2024-11-05 06:38:08 -0700

    Update cosc1010-lab08.py

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,50 +1,79 @@
-# Your Name Here
+# Logan Cabanaw
 # UWYO COSC 1010
-# Submission Date
+# 11/5/2024
 # Lab XX
-# Lab Section:
+# Lab Section: 16
 # Sources, people worked with, help given to:
 # your
 # comments
 # here
 
-
-# Write a function that will properly check strings to see if they are an int or float, and convert them if so
-# If they can't be converted return false
-# Other wise return the converted int or float 
-# Floats should only have one decimal point in them 
-
+# Q1:
+def integer_or_flt(num):
+    list(num)
+    dec_count = 0
+    symbol_count = 0
+    index = 0
+    outcome = []
+    for i in num:
+        if i.isdigit() and i != ".":
+            outcome.append("1")
+        elif i == ".":
+            dec_count += 1
+            if dec_count > 1 or index == len(num):
+                return False, 
+            outcome.append("2")
+        elif i == "-" or i == "+":
+            if index > 0:
+                return False
+        elif i.isalpha():
+            return False
+        else:
+            return False
+        index += 1
+    if "2" in outcome and "1" in outcome:
+        return(float(num))
+    elif "1" in outcome:
+        return(int(num))
+    else:
+        return False, print("5")
 
 print("*" * 75)
 
+# Q2:
+def slope_intercept(m,b,lx,ux):
+    Domain_Range = {}
+    for x in range(lx,ux):
+        Domain_Range[x] = m*x+b
+    return(Domain_Range)
 
-# Point-slope y = mx + b
-# This is used in mathematics to determine what the value y would be for any given x
-# Where b is the y-intercept, where the line crosses the y-axis (x = 0)
-# m is the slope of the line, the rate of change, how steep the line is
-# x is the variable, and is determined by which point on the graph you wish to evaluate
-# Create a function slope_intercept that takes in four parameters
-    # m, the slope
-    # b, the intercept
-    # a lower x bound
-    # an upper x bound
-# Return a list for all values of y for the given x range, inclusive (whole number X's only)
-# Check to make sure that the lower bound is less than or equal to the upper bound
-# m, b can be floats or integers
-# the bounds must be integers, if not return false
+while True:
+    m = integer_or_flt(input("(type 'exit' here to exit) m = "))
+    b = integer_or_flt(input("b = "))
+    lx = integer_or_flt(input("Lower x bound: "))
+    ux = integer_or_flt(input("Upper x bound: "))
+    if m is False or b is False or type(lx) == float or type(ux) == float:
+        print("One of your inputs are not compatable with the slope_intercept function")
+    elif m == "exit":
+        break
+    else:
+        Domain_Range = {"x","y"}
+        Domain_Range = slope_intercept(m,b,lx,ux)
+        for n in Domain_Range:
+            print(f"{n}    {Domain_Range[n]}")
 
-# Create a while loop to prompt users for their input for the four variables
-# Exit on the word exit
-# Remember all inputs are strings, but the function needs ints or floats
-# Call your function and print the resulting list
+# Q3:
 
-print("*" * 75)
-
-
-# Write a function to solve the quadratic formula
-# https://en.wikipedia.org/wiki/Quadratic_formula
-# Accept inputs for a, b, c
-# Remember that this returns two values
-# Create a loop like above to prompt the user for input for the three values
-# Create a second function that just does the square root operation 
-    # If the number you are trying to take the square root of is negative, return null
+while True:
+    a = integer_or_flt(input("(type 'exit' to exit) a = "))
+    b = integer_or_flt(input("(type 'exit' to exit) b = "))
+    c = integer_or_flt(input("(type 'exit' to exit) c = "))
+    if a is  False or b is  False or c is  False:
+        print("Something went wrong!")
+    if a == "exit" or b == "exit" or c == "exit":
+        break
+    else:
+        Disc = ((b**2) - (4*a*c))
+        x1 = round((-1*b + Disc**(1/2))/(2*a),4)
+        x2 = round((-1*b - Disc**(1/2))/(2*a),4)
+        print(f"{x1}, {x2}")
